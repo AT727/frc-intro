afb2e6e181afdef7acb8d61413de629505c4ea62
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-20 15:23:15 -0400

    Update RunIntake.java

--- src/main/java/frc/robot/subsystems/Intake/Command/RunIntake.java
+++ src/main/java/frc/robot/subsystems/Intake/Command/RunIntake.java
@@ -1,5 +1,9 @@
 class RunIntake{
-  
+   public CANSparkMax intake; 
+
+  public RunIntake(){
+    this.intake = new CANSparkMax(Motor.kPort, CANSparkMax.MotorType.kBrushless);   
+  }
   
   public void setPower(double power){
     this.intake.set(power);
