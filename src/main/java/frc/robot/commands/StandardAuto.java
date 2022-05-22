5fc7e325324b2e24b0668883c4e4bbdfcc7dfb01
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-22 15:35:10 -0400

    Update StandardAuto.java

--- src/main/java/frc/robot/commands/StandardAuto.java
+++ src/main/java/frc/robot/commands/StandardAuto.java
@@ -10,46 +10,20 @@ import frc.robot.subsystems.drivetrain.commands.TurnByAngle;
 import frc.robot.subsystems.drivetrain.commands.TurnToAngle;
 
 public class StandardAuto extends SequentialCommandGroup {
-    public StandardAuto(Drivetrain drivetrain){
+    public StandardAuto(Intake intake){
         addCommands(
-            // Initialize by turning to start heading
-            // Turn robot to 0 degrees
-            new TurnToAngle(drivetrain, 0.0),
-
-            // First iteration: 0 to 90 degrees
-            // Drive robot forward at a safe speed for for 2 seconds
-            new ParallelRaceGroup(
-                new RunCommand(() -> {
-                    drivetrain.drive(Constants.Drivetrain.kSafeForwardThrottle, 0);
-                }, drivetrain),
-                new WaitCommand(2.0)
-            ),
-            // Turn robot by 90 degrees
-            new TurnByAngle(drivetrain, 90.0),
-
-            // Second iteration: 90 to 180 degrees
-            // Drive robot forward at a safe speed for for 2 seconds
-            new ParallelRaceGroup(
-                new RunCommand(() -> {
-                    drivetrain.drive(Constants.Drivetrain.kSafeForwardThrottle, 0);
-                }, drivetrain),
-                new WaitCommand(2.0)
-            ),
-            // Turn robot by 90 degrees
-            new TurnByAngle(drivetrain, 90.0),
-
-            // Third iteration: 180 to 270 degrees
-            // Drive robot forward at a safe speed for for 2 seconds
-            new ParallelRaceGroup(
-                new RunCommand(() -> {
-                    drivetrain.drive(Constants.Drivetrain.kSafeForwardThrottle, 0);
-                }, drivetrain),
-                new WaitCommand(2.0)
-            ),
-            // Turn robot by 90 degrees
-            new TurnByAngle(drivetrain, 90.0)
+          new RunCommand(() ->
+            {
+              intake.execute(1);
+            }),
+          
+          new RunCommand(()->
+            {
+              intake.end();
+            })
+          
         );
 
-        addRequirements(drivetrain);
+        addRequirements(intake);
     }
 }
