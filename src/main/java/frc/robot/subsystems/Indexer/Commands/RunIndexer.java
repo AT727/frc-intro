219bcde04001d14fe0811764b3c348613397dab0
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-31 21:19:58 -0400

    Update RunIndexer.java

--- src/main/java/frc/robot/subsystems/Indexer/Commands/RunIndexer.java
+++ src/main/java/frc/robot/subsystems/Indexer/Commands/RunIndexer.java
@@ -1,3 +1,29 @@
 class RunIndexer extends CommandBase{
+public Indexer indexer;
+  
+public RunIndexer(){
+  this.indexer = new Indexer();
+}
+  
+  @Override
+    public void init() {
 
+    }   
+  
+    @Override
+    public void execute(double power) {
+        indexer.ascend();
+        indexer.descend();
+    }   
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+  
+   @Override
+    public void end() {
+        indexer.stop();
+    }
+  
 }
