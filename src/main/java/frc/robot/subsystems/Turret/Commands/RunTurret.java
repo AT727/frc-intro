38b3bc0f09c42d86f8280522e7b679996c058f09
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-31 21:11:05 -0400

    Update RunTurret.java

--- src/main/java/frc/robot/subsystems/Turret/Commands/RunTurret.java
+++ src/main/java/frc/robot/subsystems/Turret/Commands/RunTurret.java
@@ -1,1 +1,32 @@
 
+class RunTurret extends CommandBase {
+  public Turret turret;
+  public double highAngle;
+  public double lowAngle;
+  
+  public RunTurret(){
+  this.turret = new Turret();
+  }
+  
+ @Override
+    public void init() {
+
+    }   
+  
+    @Override
+    public void execute(double power) {
+        turret.setAngle(highAngle);
+        turret.setPower(0.5);
+    }   
+
+    @Override
+    public boolean isFinished() {
+        turret.encoder.getAngle() - turret.encoder.getTargetAngle() == 0;
+    }
+  
+   @Override
+    public void end() {
+        turret.setPower(0);
+    }
+}
+  
