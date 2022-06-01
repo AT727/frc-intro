8b2f5ae9409448538f92b3145bf6ed92941e63e4
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-31 21:03:17 -0400

    Create Turret.java

---
+++ src/main/java/frc/robot/subsystems/Turret/Turret.java
@@ -0,0 +1,22 @@
+import com.revrobotics.CANSparkMax;
+
+class Turret {
+   private CANSparkMax angleMotor;
+   private CANSparkMax speedMotor;
+   private RelativeEncoder encoder;
+  
+  public Turret(){
+    this.angleMotor = new CANSparkMax(Motor.0, CANSparkMax.MotorType.kBrushless);
+    this.speedMotor = new CANSparkMax(Motor.1, CANSparkMax.MotorType.kBrushless);
+    this.encoder = this.angleMotor.getEncoder();
+  }
+  
+  public void setAngle(double angle){
+    this.encoder.setPosition(angle);
+  }
+  
+  public void setSpeed(double power){
+    this.speedMotor.setPower(power);
+  }
+    
+}
